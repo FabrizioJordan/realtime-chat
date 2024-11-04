b7e7c41bed8ffb0cb21ff114b0b979285d43969a
FabricioJordan <fabriorange28@gmail.com>
2024-11-03 22:53:40 -0300

    "Index.html created and connected with app"

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
+import logger from "morgan"
 
 const port = process.env.port ?? 3000 
 
 const app = express()
+app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-    res.send('Hola Chat!')
+    res.sendFile(process.cwd() + '/client/index.html')
 })
 
 app.listen(port, () => {
