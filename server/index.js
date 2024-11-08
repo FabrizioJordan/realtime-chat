60de61bb660a4773b38e3852e798824b918bb370
FabricioJordan <fabriorange28@gmail.com>
2024-11-07 23:23:14 -0300

    "Recover messages created and completed"

--- server/index.js
+++ server/index.js
@@ -30,28 +30,48 @@ const db = createClient({
 await db.execute(`
 CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT
+    content TEXT,
+    username TEXT
 )
     `)
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('a user has connected')
 
     socket.on('disconnect', () => console.log("an user has disconnected"))
 
     socket.on('chat message', async (msg) => {
         let result 
+        const username = socket.handshake.auth.username ?? 'anonymous'
+        
         try {
+
             result = await db.execute({
-                sql: `INSERT INTO messages (content) VALUES (:msg)`,
-                args: { msg }
+                sql: `INSERT INTO messages (content, username) VALUES (:msg, :username)`,
+                args: { msg, username }
             })
         } catch(e){
             console.error(e)
             return 
         }
-        io.emit('chat message', msg, result.lastInsertRowid.toString())
+
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
     })
+
+    if(!socket.recovered){
+        try{
+            const results = await db.execute({
+                sql: 'SELECT id, content, username FROM messages WHERE id > ?',
+                args: [socket.handshake.auth.serverOffset ?? 0]
+            })
+            results.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id, row.username)
+            })
+        } catch(err){
+            console.error(err)
+        }
+    }
+
 })
 
 app.use(logger('dev'))
