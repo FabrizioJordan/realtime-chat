fbf45e55929b3715702cdc717664a7a988356169
FabricioJordan <fabriorange28@gmail.com>
2024-11-13 20:27:26 -0300

    "Updated HTML, CSS, and JavaScript files to modify layout, styles, and functionality of a chat application, including adding dark mode support and menu toggling."

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./client/*.{html,js}'],
+  darkMode: 'selector',
   theme: {
     extend: {
       colors: {
       'orange': '#ffa500',
-    },
+      },
       spacing: {
         '8xl': '96rem',
         '9xl': '128rem',
       },
       borderRadius: {
         '4xl': '2rem',
+      },
+      translate: {
+        'full': '100vw',
+      },
+      width: {
+        'min-menu': '5%',
+        'mid-menu': '25%'
       }
     }
   },
